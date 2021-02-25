877ee4f461b2624d51a74b57ef08fba445ca8a7f
Diego G Plata <diegogplata@gmail.com>
2021-02-25 16:50:14 -0600

    💥introduce firebase backend for login🛂🚧work in progress

---
+++ lib/services/auth.dart
@@ -0,0 +1,20 @@
+//import firebase auth
+
+//import 'package:my_tdd_devops_mobile/models/user.dart';
+
+class AuthService {
+  //final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user object based on FirebaseUser
+
+  /*User _userFromFirebaseUSer(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }*/
+
+  // auth change user stream
+  /*Stream<User> get user {
+    return _auth.onAuthStateChanged
+        //.map((FirebaseUser user) => _userFromFirebaseUSer(user));
+        .map(_userFromFirebaseUSer);
+  }*/
+}
